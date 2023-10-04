90df9981b3a616de0800095fa0a07f7612d7591f
Oleksii Karpenko <alexkarpenko@yahoo.com>
2023-10-04 09:15:42 +0300

    Add draft context implementation

---
+++ src/context.rs
@@ -0,0 +1,66 @@
+use once_cell::sync::OnceCell;
+use std::collections::HashMap;
+use std::env;
+use tracing::debug;
+
+const CTX_APP_NAME: &str = env!("CARGO_PKG_NAME");
+const CTX_APP_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub type ContextMap = HashMap<String, String>;
+
+#[derive(Debug)]
+pub struct Context<'a> {
+    own: ContextMap,
+    parent: Option<&'a Context<'a>>,
+}
+
+impl<'a> Context<'a> {
+    pub fn root() -> &'static Context<'a> {
+        static ROOT_CONTEXT: OnceCell<Context> = OnceCell::new();
+
+        let ctx = ROOT_CONTEXT.get_or_init(|| {
+            let mut ctx = ContextMap::new();
+
+            ctx.insert("CTX_APP_NAME".into(), CTX_APP_NAME.into());
+            ctx.insert("CTX_APP_VERSION".into(), CTX_APP_VERSION.into());
+
+            env::vars().for_each(|(k, v)| {
+                ctx.insert(k, v);
+            });
+
+            let ctx = Context {
+                own: ctx,
+                parent: None,
+            };
+
+            debug!("Construct new root context: {:#?}", ctx);
+            ctx
+        });
+
+        ctx
+    }
+
+    pub fn with(&self, own: ContextMap) -> Context {
+        Context {
+            own,
+            parent: Some(self),
+        }
+    }
+
+    pub fn get(&self, var: &String) -> Option<&String> {
+        // Try own context
+        if let Some(value) = self.own.get(var) {
+            Some(value)
+            // or get parent if it's
+        } else if let Some(parent) = &self.parent {
+            // and try parent
+            if let Some(value) = parent.get(var) {
+                Some(value)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 mod cli;
 mod config;
+mod context;
 mod errors;
 
 use cli::CliConfig;
 use config::AppConfig;
+use context::Context;
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let cli_config = CliConfig::new();
     let app_config = AppConfig::from(&cli_config.config)?;
+    let ctx = Context::root();
 
     Ok(())
 }
