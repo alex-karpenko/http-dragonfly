b9b02de88b691de04e1102c804cfe3cd25fa8d3c
Oleksii Karpenko <alexkarpenko@yahoo.com>
2024-01-20 22:14:44 +0200

    Add some trivial integration tests

---
+++ tests/basic_config.rs
@@ -0,0 +1,93 @@
+mod common;
+
+use crate::common::run_test_with_config;
+use insta::assert_debug_snapshot;
+
+const TEST_CONFIG_PATH: &str = "tests/configs/integration/basic.yaml";
+
+#[tokio::test]
+async fn basic_functionality() {
+    let result = run_test_with_config(TEST_CONFIG_PATH, 3000, 60, async {
+        let client = reqwest::Client::new();
+        // The simplest request
+        assert_eq!(
+            client
+                .get("http://localhost:8000/")
+                .send()
+                .await
+                .unwrap()
+                .status(),
+            200,
+            "simple request to 8000, 200 expected"
+        );
+        assert_debug_snapshot!(
+            client
+                .get("http://localhost:8000/5")
+                .send()
+                .await
+                .unwrap()
+                .text()
+                .await
+        );
+
+        // 502 WRONG
+        assert_eq!(
+            client
+                .get("http://localhost:8001/")
+                .send()
+                .await
+                .unwrap()
+                .status(),
+            502,
+            "request to invalid target, 502 expected"
+        );
+        assert_debug_snapshot!(
+            client
+                .get("http://localhost:8001/")
+                .send()
+                .await
+                .unwrap()
+                .text()
+                .await
+        );
+
+        // 200 GOOD
+        assert_eq!(
+            client
+                .get("http://localhost:8002/")
+                .send()
+                .await
+                .unwrap()
+                .status(),
+            200,
+            "request to valid target, 200 expected"
+        );
+        assert_debug_snapshot!(
+            client
+                .get("http://localhost:8002/")
+                .send()
+                .await
+                .unwrap()
+                .text()
+                .await
+        );
+
+        // Request with timeout
+        assert_eq!(
+            client
+                .get("http://localhost:8000/")
+                .header("X-Delay", "2")
+                .send()
+                .await
+                .unwrap()
+                .status(),
+            504,
+            "request to 8000 with timeout, 504 is expected"
+        );
+
+        //
+    })
+    .await;
+
+    assert_eq!(result, Ok(()))
+}
