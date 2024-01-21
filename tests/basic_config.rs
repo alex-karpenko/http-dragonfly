2a01733ef962ae05cd0d283d47e3e22f5142d17d
Oleksii Karpenko <alexkarpenko@yahoo.com>
2024-01-21 13:16:52 +0200

    Add filed first tests

--- tests/basic_config.rs
+++ tests/basic_config.rs
@@ -42,47 +42,96 @@ async fn basic_functionality() {
                 .await
         );
 
-        // 502 WRONG
+        // failed_then_ok
+        let wrong = client.get("http://localhost:8001/").send().await.unwrap();
+        let good = client
+            .get("http://localhost:8001/")
+            .header("x-skip-wrong", "yes")
+            .send()
+            .await
+            .unwrap();
+        let timeout = client
+            .get("http://localhost:8001/")
+            .header("x-skip-wrong", "yes")
+            .header("x-include-timeout", "yes")
+            .send()
+            .await
+            .unwrap();
         assert_eq!(
-            client
-                .get("http://localhost:8001/")
-                .send()
-                .await
-                .unwrap()
-                .status(),
+            wrong.status(),
             502,
             "request to invalid target, 502 expected"
         );
-        assert_debug_snapshot!(
-            client
-                .get("http://localhost:8001/")
-                .send()
-                .await
-                .unwrap()
-                .text()
-                .await
-        );
-
-        // 200 GOOD
+        assert_eq!(good.status(), 200, "request to valid target, 200 expected");
         assert_eq!(
-            client
-                .get("http://localhost:8002/")
-                .send()
-                .await
-                .unwrap()
-                .status(),
-            200,
-            "request to valid target, 200 expected"
+            timeout.status(),
+            504,
+            "request to delayed target, 504 expected"
         );
-        assert_debug_snapshot!(
-            client
-                .get("http://localhost:8002/")
-                .send()
-                .await
-                .unwrap()
-                .text()
-                .await
+        assert_debug_snapshot!(wrong.headers().get("x-target-id"));
+        assert_debug_snapshot!(good.headers().get("x-target-id"));
+        assert_debug_snapshot!(timeout.headers().get("x-target-id"));
+
+        // failed_then_target_id
+        let wrong = client.get("http://localhost:8002/").send().await.unwrap();
+        let good = client
+            .get("http://localhost:8002/")
+            .header("x-skip-wrong", "yes")
+            .send()
+            .await
+            .unwrap();
+        let timeout = client
+            .get("http://localhost:8002/")
+            .header("x-skip-wrong", "yes")
+            .header("x-include-timeout", "yes")
+            .send()
+            .await
+            .unwrap();
+        assert_eq!(
+            wrong.status(),
+            502,
+            "request to invalid target, 502 expected"
         );
+        assert_eq!(good.status(), 200, "request to valid target, 200 expected");
+        assert_eq!(
+            timeout.status(),
+            504,
+            "request to delayed target, 504 expected"
+        );
+        assert_debug_snapshot!(wrong.headers().get("x-target-id"));
+        assert_debug_snapshot!(good.headers().get("x-target-id"));
+        assert_debug_snapshot!(timeout.headers().get("x-target-id"));
+
+        // failed_then_override
+        let wrong = client.get("http://localhost:8003/").send().await.unwrap();
+        let good = client
+            .get("http://localhost:8003/")
+            .header("x-skip-wrong", "yes")
+            .send()
+            .await
+            .unwrap();
+        let timeout = client
+            .get("http://localhost:8003/")
+            .header("x-skip-wrong", "yes")
+            .header("x-include-timeout", "yes")
+            .send()
+            .await
+            .unwrap();
+        assert_eq!(
+            wrong.status(),
+            502,
+            "request to invalid target, 502 expected"
+        );
+        assert_eq!(good.status(), 200, "request to valid target, 200 expected");
+        assert_eq!(
+            timeout.status(),
+            504,
+            "request to delayed target, 504 expected"
+        );
+        assert_debug_snapshot!(wrong.headers().get("x-target-id"));
+        assert_debug_snapshot!(good.headers().get("x-target-id"));
+        assert_debug_snapshot!(timeout.headers().get("x-target-id"));
+
     })
     .await;
 
